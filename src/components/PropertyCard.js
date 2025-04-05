92eca556ce7e14bf11572147752ab525cb8823d1
Erdauit Torekhan <98638156+Erdauit@users.noreply.github.com>
2025-04-06 03:43:41 +0500

    Add files via upload

--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function PropertyCard({ property, onClick }) {
     return (
         <div style={{ width: 200, margin: 10, border: '1px solid #ccc', padding: 10 }} onClick={() => onClick(property)}>
-            <img src={`http://localhost:8000${property.image}`} alt={property.title} width="100%" />
+            <img src={`${process.env.REACT_APP_API_URL}${property.image}`} alt={property.title} width="100%" />
             <h3>{property.title}</h3>
             <p>{property.complex_name}</p>
         </div>
